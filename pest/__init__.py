b67251b9c895b24c84118b62cea9073094187bb2
Lucas Colombo <lucasncolombo@gmail.com>
2023-12-23 22:22:39 -0300

    refactor: 🔨 improve public api

--- pest/__init__.py
+++ pest/__init__.py
@@ -1,5 +1,28 @@
+from .decorators.controller import api, controller, ctrl, router, rtr
+from .decorators.handler import delete, get, head, options, patch, post, put, trace
+from .decorators.module import dom, domain, mod, module
 from .factory import Pest
 
 __all__ = [
     'Pest',
+    # decorators - module
+    'module',
+    'mod',
+    'domain',
+    'dom',
+    # decorators - handler
+    'get',
+    'post',
+    'put',
+    'delete',
+    'patch',
+    'options',
+    'head',
+    'trace',
+    # decorators - controller
+    'controller',
+    'ctrl',
+    'router',
+    'rtr',
+    'api',
 ]
