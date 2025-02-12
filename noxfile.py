3d3f0065c4023a1c89748608cb6428752b5c9aaa
Lucas Colombo <lucasncolombo@gmail.com>
2025-02-12 12:43:29 -0300

    chore: 🧹 update dependencies

--- noxfile.py
+++ noxfile.py
@@ -1,15 +1,7 @@
 from nox_poetry import Session, session
 
 
-@session(
-    python=[
-        '3.12',
-        '3.11',
-        '3.10',
-        '3.9',
-        '3.8',
-    ]
-)
+@session(python=['3.13', '3.12', '3.11', '3.10', '3.9'])
 def tests(session: Session) -> None:
     session.install(
         '.',
